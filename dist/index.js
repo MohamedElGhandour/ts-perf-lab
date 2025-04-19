1bb905452a8553ad811c115015ba6328cf702488
mohamed elghandour <m.yahya1728@gmail.com>
2025-04-19 20:24:46 +0200

    (chore): update .gitignore

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const reverse_1 = require("./functions/reverse");
-const subString_1 = require("./functions/subString");
-(0, reverse_1.reverseBenchmark)();
-(0, subString_1.substringBenchmark)();
